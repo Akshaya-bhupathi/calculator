9bb40425ad558aba99667b22b4b9eafa1a09855b
Akshaya-bhupathi <86120714+Akshaya-bhupathi@users.noreply.github.com>
2025-03-21 16:09:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ let string = ""
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
-        if(e.target.innerHTML == '='){
+        if(e.target.innerHTML === '='){
             string = eval(string);
             document.querySelector('input').value = string;
         }
-        else if(e.target.innerHTML == 'AC'){
+        else if(e.target.innerHTML === 'AC'){
             string = "";
             document.querySelector('input').value = string;
         }
-        else if(e.target.innerHTML == 'C'){
+        else if(e.target.innerHTML === 'C'){
             string = string.slice(0,-1);
             document.querySelector('input').value = string;
         }
